79b2972fed79469135d13a1eb5a9e4444860d48c
Tom Mornini <tom@subledger.com>
2015-05-19 18:04:01 -0700

    introducing TicTacToe

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 # -*- encoding : utf-8 -*-
 
+puts 'Welcome to Tic-Tac-Toe!'
 
-puts 'Hello World!'
+class TicTacToe
+  def self.play
+    puts 'Shall we play?'
+  end
+end
+
+TicTacToe.play
