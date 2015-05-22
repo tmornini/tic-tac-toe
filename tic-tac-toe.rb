597b4b0152c9a17ebd071b2678305b13c2749e4b
Tom Mornini <tom@subledger.com>
2015-05-21 18:53:06 -0700

    improved TicTacToe

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -3,10 +3,28 @@
 
 puts 'Welcome to Tic-Tac-Toe!'
 
-class TicTacToe
-  def self.play
-    puts 'Shall we play?'
+module TicTacToe
+  class Game
+    def initialize board, player_1, player_2
+      @board    = board
+      @player_1 = player_1
+      @player_2 = player_2
+    end
+
+    def play
+      puts 'Shall we play?'
+    end
   end
 end
 
-TicTacToe.play
+board    = nil
+player_1 = nil
+player_2 = nil
+
+a_game = TicTacToe::Game.new board, player_1, player_2
+b_game = TicTacToe::Game.new 'board', 'player_1', 'player_2'
+
+puts a_game
+puts b_game
+
+a_game.play
