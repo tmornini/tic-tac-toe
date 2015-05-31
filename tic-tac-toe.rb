bf6fc21811c82ba4756824cdca2503acbc77416f
Tom Mornini <tom@subledger.com>
2015-05-31 15:39:30 -0700

    working board

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -12,7 +12,8 @@ module TicTacToe
     end
 
     def play
-      puts "board:    #{@board}"
+      puts "board:"
+      puts @board
       puts "player_1: #{@player_1}"
       puts "player_2: #{@player_2}"
       puts 'Shall we play?'
@@ -20,14 +21,28 @@ module TicTacToe
   end
 
   class Board
+    def initialize
+      @cells = [ [ ' ', ' ', ' ' ],
+                 [ ' ', ' ', ' ' ],
+                 [ ' ', ' ', ' ' ] ]
+    end
+
+    def cell_is_an_x x, y
+      @cells[x][y] = 'x'
+    end
+
+    def cell_is_an_o x, y
+      @cells[x][y] = 'o'
+    end
+
     def to_s
-      '-------' \
-      '| | | |' \
-      '-------' \
-      '| | | |' \
-      '-------' \
-      '| | | |' \
-      '-------'
+      "-------\n"                                           \
+      "|#{@cells[0][0]}|#{@cells[1][0]}|#{@cells[2][0]}|\n" \
+      "-------\n"                                           \
+      "|#{@cells[0][1]}|#{@cells[1][1]}|#{@cells[2][1]}|\n" \
+      "-------\n"                                           \
+      "|#{@cells[0][2]}|#{@cells[1][2]}|#{@cells[2][2]}|\n" \
+      "-------\n"
     end
   end
 
@@ -41,5 +56,9 @@ module TicTacToe
     end
   end
 
-  Game.new(Board.new, Player.new('Tom'), Player.new('John')).play
+  board = Board.new
+
+  board.cell_is_an_o 1, 1
+
+  Game.new(board, Player.new('Tom'), Player.new('John')).play
 end
