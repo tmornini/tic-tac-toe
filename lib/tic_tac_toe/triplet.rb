ac65e6fc2bb6a092a2d37f2a8676c1827106dec4
Tom Mornini <tom@subledger.com>
2015-06-06 14:36:27 -0700

    Introducing Triplet

---
+++ lib/tic_tac_toe/triplet.rb
@@ -0,0 +1,11 @@
+# -*- encoding : utf-8 -*-
+
+module TicTacToe
+  class Triplet
+    def initialize cell_0, cell_1, cell_2
+      @cell_0 = cell_0
+      @cell_1 = cell_1
+      @cell_2 = cell_2
+    end
+  end
+end
---
+++ lib/tic_tac_toe/triplet.spec
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+
+require '_spec/_helpers'
+
+require 'tic_tac_toe/triplet'
+
+module TicTacToe
+  Class Triplet do
+  end
+end
